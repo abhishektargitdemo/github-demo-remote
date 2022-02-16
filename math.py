8acaba04991c7be0ca905efa0ab4ee2502a4a8d0
abhishektargitdemo <99796658+abhishektargitdemo@users.noreply.github.com>
2022-02-16 15:37:05 +0530

    initian commit of math.py

---
+++ math.py
@@ -0,0 +1,16 @@
+#Add Implementation
+def add(x,y):
+	return x+y
+#subtract Implementation
+def subtract(x,y):
+	return x-y #on mster
+#multiply Implementation
+def multiply(x,y):
+	return x*y # on bug456 branch
+#divide Implementation
+def divide(x,y):
+	if y==0:  #master branch
+    return DIVIDEBY0ERROR
+    else:
+    return x/y
+
